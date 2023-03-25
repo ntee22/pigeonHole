dada8f1ce83d23858ae1d91fd9b7a24424206b61
Nhut <nhut.nguyen@pandora.digital>
2023-03-25 17:58:39 +0700

    Push version 0.1

---
+++ src/test/java/com/pigeonhole/pigeonhole/pigeonHoleClass.java
@@ -0,0 +1,130 @@
+package com.pigeonhole.pigeonhole;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+
+public class pigeonHoleClass {
+    private final WebDriver driver;
+
+    public pigeonHoleClass(WebDriver driver){
+        this.driver = driver;
+    }
+
+    public void goToHomePage(){
+        driver.get("https://pigeonhole.at");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.titleContains("Pigeonhole"));
+    }
+
+    public void enterPasscode(String passCode) throws InterruptedException {
+        WebElement eventPasscodeField = driver.findElement(By.xpath("//*[@id=\"passcodeInput\"]"));
+        eventPasscodeField.sendKeys(passCode);
+        eventPasscodeField.sendKeys(Keys.ENTER);
+        Thread.sleep(2000);
+
+        // Wait for the attendeeCodeField to be visible
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement attendeeCodeField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"attendee-code-input\"]")));
+
+        // Check that the attendee code input field is displayed
+        if (attendeeCodeField.isDisplayed()) {
+            System.out.println("Attendee Code Field is displayed");
+        }else {
+            System.out.println("Attendee Code Field is not displayed");
+        }
+    }
+
+    public void enterAttendeeCode(String attendeeCode) throws InterruptedException {
+        WebElement attendeeCodeField = driver.findElement(By.xpath("//*[@id=\"attendee-code-input\"]"));
+        attendeeCodeField.sendKeys(attendeeCode);
+        attendeeCodeField.sendKeys(Keys.ENTER);
+        Thread.sleep(2000);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.titleContains("Pigeonlab"));
+    }
+
+    public void enterQA (){
+        WebElement qaSessionButton = driver.findElement(By.xpath("//*[contains(text(), 'Enter this Q&A')]"));
+        qaSessionButton.click();
+        //*[@id="tabpanel-"]/div[2]/div/nav/div[2]/div[1]/section/div/div[5]/a/span[1]
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.titleContains("Q&A"));
+    }
+
+    public void askQuestion(String question) throws InterruptedException {
+        WebElement askQuestionField = driver.findElement(By.xpath("//div[2]/div[2]/div/div[2]/div[1]/textarea"));
+        askQuestionField.sendKeys(question);
+        Thread.sleep(2000);
+        WebElement askQuestionButton = driver.findElement(By.xpath("//div[2]/div[2]/div[2]/button/div"));
+        askQuestionButton.click();
+        Thread.sleep(2000);
+        // Get text of question asked
+        WebElement questionAsked = driver.findElement(By.xpath("//div[6]/div/section/div[3]/div[1]/div"));
+        String questionAskedText = questionAsked.getText();
+        // Check if the comment is displayed at the top of the list
+        if (questionAskedText.equals(question)) {
+            System.out.println("Question is asked");
+        } else {
+            System.out.println("Question is not asked");
+        }
+//        WebElement questionAsked = driver.findElement((By.xpath("//*[@id=\"question-19588481\"]")));
+//        String questionAskedSent = questionAsked.getText();
+//        Assert.assertEquals(questionAsked, questionAskedSent);
+    }
+
+    public void clickToComment() throws InterruptedException {
+        WebElement clickCommentButton = driver.findElement(By.xpath("//div[3]/div[1]/div/div[2]/div[2]/a[2]/span[2]/span[2]"));
+        clickCommentButton.click();
+        Thread.sleep(3000);
+    }
+
+    public void addComment(String comment) throws InterruptedException {
+        WebElement addCommentField = driver.findElement(By.xpath("//div/div[1]/div[1]/textarea"));
+        addCommentField.sendKeys(comment);
+        WebElement commentButton = driver.findElement(By.xpath("//div/div[1]/div[2]/div[2]/div[2]/button"));
+        commentButton.click();
+        Thread.sleep(2000);
+        // Get text that commented
+        WebElement commented = driver.findElement(By.xpath("//div/div[2]/div/div/div/div[2]/p"));
+        String textCommented = commented.getText();
+        // Check if the comment is displayed at the top of the list
+        if (textCommented.equals(comment)) {
+            System.out.println("Comment is displayed");
+        } else {
+            System.out.println("Comment is not displayed");
+        }
+    }
+
+    public void clickUpvote(){
+        // Find upvote button
+        WebElement upvoteButton = driver.findElement(By.xpath("//div[1]/div[3]/button[1]/span[1]"));
+
+        //  Get the initial upvote count
+        WebElement upvoteCount = driver.findElement(By.xpath("//div/div[2]/div/div/div[1]/div[3]/button[1]"));
+        int initBeforeUpvoteCount = Integer.parseInt(upvoteCount.getText());
+
+        // Click button upvote
+        upvoteButton.click();
+
+        // Get the initial after click upvote
+        WebElement afterUpvoteCount = driver.findElement(By.xpath("//div/div[2]/div/div/div[1]/div[3]/button[1]"));
+        int initAfterUpvoteCount = Integer.parseInt(afterUpvoteCount.getText());
+
+        // Verify upvote count increased 1
+        if (initAfterUpvoteCount == initBeforeUpvoteCount + 1) {
+            System.out.println("Upvote is increase");
+        } else {
+            System.out.println("Upvote is not increase");
+        }
+    }
+
+    public void quitDriver(){
+        driver.quit();
+    }
+    }
+
---
+++ src/test/java/com/pigeonhole/pigeonhole/pigeonHoleMain.java
@@ -0,0 +1,42 @@
+package com.pigeonhole.pigeonhole;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class pigeonHoleMain {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "/Users/kelvin/Nhut/AquaProjects/pigeonhole/drivers/chromedriver");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(options);
+        pigeonHoleClass pWeb = new pigeonHoleClass(driver);
+
+        // 1. Go to https://pigeonhole.at (Make sure the page is loaded)
+        pWeb.goToHomePage();
+
+        // 2. Type in event passcode and click on the submit button (Attendee code field should appear)
+        pWeb.enterPasscode("PIGEONQATEST");
+
+        // 3.  Type in the attendee code and click on the submit button (The page should navigate to an agenda arrow button page)
+        pWeb.enterAttendeeCode("0752I6SHZN");
+
+        // 4. Enter Q&A (The page should navigate into the Q&A button. session.)
+        pWeb.enterQA();
+
+        // 5. Type in the event passcode and click on the `Ask` button. (A popup modal should appear, and you should see your question posted in the list)
+        pWeb.askQuestion("Test ask");
+
+        // 6. Click on the `Add a comment` button under your question (The page should navigate to the comments page)
+        pWeb.clickToComment();
+
+        // 7. Type in the event passcode and click on the comment button (The popup should appear and close. Your comment appears in the list)
+        pWeb.addComment("Test comment");
+
+        // 8. Upvote on the comment you have just posted (The comment upvote count should increase by 1)
+        pWeb.clickUpvote();
+
+        // Done TestCase
+        pWeb.quitDriver();
+    }
+}
